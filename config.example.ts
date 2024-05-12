e81c95e1846a64b35fa01514482f12cb4661439e
ZxStim <thang.victor.luong@gmail.com>
2024-05-12 19:06:54 +0700

    set up test

---
+++ config.example.ts
@@ -0,0 +1,5 @@
+export const Config = {
+  apiKey: "INSERT_API_KEY",
+  test1: "INSERT_ADDRESS",
+  test2: ["INSERT_ADDRESS_1", "INSERT_ADDRESS_2"]
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,24 @@
-import {Etherscan} from './src/scans.ts'
+import { Etherscan } from './src/scans'
+// For testing, I use config.ts file to store configurations like API key and test addresses
+import { Config } from './config'
+
+const etherscan = new Etherscan(Config.apiKey || "")
+
+// Test getAccounts
+etherscan.getAccounts(Config.test1)
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// Test getAccounts for multiple addresses
+etherscan.getAccounts(Config.test2)
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+

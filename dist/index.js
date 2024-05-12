c7f2fdbf7203b5c88cd4c7edaf24a510b51bc7a1
ZxStim <thang.victor.luong@gmail.com>
2024-05-12 16:54:07 +0700

    add gitignore

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Etherscan = void 0;
-var scans_1 = require("./scans");
-Object.defineProperty(exports, "Etherscan", { enumerable: true, get: function () { return scans_1.Etherscan; } });
--- dist/scans.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Etherscan {
-    apiKey: string;
-    rateLimit: number;
-    constructor(apiKey: string, rateLimit: number);
-    getAccounts(address: string | string[]): Promise<any>;
-    getERC20AccountBalance(address: string, contractAddress: string): Promise<any>;
-}
